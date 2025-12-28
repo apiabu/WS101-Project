12605a4164ff0c8333ea27227214ca1f272fba4b
Theo-Cent <centinotheo23@gmail.com>
2025-12-28 16:21:52 +0800

    Managed database queries and data processing

--- src/main/java/com/example/lendingsystem/repository/UserRepository.java
+++ src/main/java/com/example/lendingsystem/repository/UserRepository.java
@@ -7,3 +7,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
 }
+//Managed database queries and data processing - jiro theo vacunawa
